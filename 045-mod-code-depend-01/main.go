64ec6b3c13a3970dda54bc94574452565646aa5e
a7medalyapany <ahmedalyapany1@gmail.com>
2025-10-05 20:46:52 +0300

    Feat: Add initial implementation with puppy dependency

---
+++ 045-mod-code-depend-01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/a7medalyapany/puppy"
+)
+
+func main() {
+	barks := puppy.Barks()
+	fmt.Println(puppy.Bark())
+	fmt.Println(barks)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module GoToH
+
+go 1.25.1
+
+require github.com/a7medalyapany/puppy v0.0.0-20251005121232-f37093b18474
