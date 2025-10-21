271044221a0be77fc899e9d90cf4e700b7b8b6b9
a7medalyapany <ahmedalyapany1@gmail.com>
2025-10-21 22:50:45 +0300

    feat: Add demonstration of slice creation and capacity in Go

--- 055-hands-on-exercises-03/main.go
+++ 055-hands-on-exercises-03/main.go
@@ -24,4 +24,14 @@ func main() {
 	// deleting and slicing
 	x = append(x[:3], x[6:10]...)
 	fmt.Println(x)
+
+
+	fmt.Println("------------------")
+	states := make([]string, 0, 20)
+	states = append(states,` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`)
+	
+	fmt.Printf("the len - %v\nthe cap - %v\nand the slice holds -  %v\n", len(states), cap(states), states)
+
+	
+	fmt.Println("------------------")
 }
