2ba7ca6486d4fd28d6945ba3a2dc430d63cfca40
a7medalyapany <ahmedalyapany1@gmail.com>
2025-10-28 21:24:29 +0300

    feat: Implement person struct and main function with ice cream flavor demonstration

---
+++ 060-hands-on-exercises-05/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName       string
+	lastName        string
+	iceCreamFlavors []string
+}
+
+func main() {
+
+	p1 := person{
+		firstName:       "Bob",
+		lastName:        "Brown",
+		iceCreamFlavors: []string{"Vanilla", "Chocolate", "Strawberry"},
+	}
+
+	fmt.Println("the person struct is - ", p1)
+	fmt.Println("Ice Cream Flavors:", p1.iceCreamFlavors)
+	fmt.Printf("the type of the p1 is %T & the type of the iceCreamFlavors variable is %T\n", p1, p1.iceCreamFlavors)
+
+	for i, flavor := range p1.iceCreamFlavors {
+		fmt.Printf("the ice cream flavor number %v is %v\n", i+1, flavor)
+	}
+}
