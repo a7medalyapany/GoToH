c5a05a569849ec57a7250acb128a395fe3effeca
a7medalyapany <ahmedalyapany1@gmail.com>
2025-10-31 14:45:49 +0200

    refactor: Remove file writing logic and unused types from main function

--- 065-writer-interface-file/main.go
+++ 065-writer-interface-file/main.go
@@ -3,37 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
-	"os"
 )
 
-// type person struct {
-// 	firstName string
-// }
-
-// func (p person) writeOut(w io.Writer) error {
-// 	_, err := io.WriteString(w, "Hello, "+p.firstName)
-// 	return err
-// }
-
 func main() {
-	f, err := os.Create("output.txt")
-	if err != nil {
-		log.Fatalf("Could not create file: %v", err)
-	}
 
-	defer f.Close()
-
-	s := []byte("Hello, World!")
-
-	_, err = f.Write(s)
-
-	if err != nil {
-		log.Fatalf("Could not write to file: %v", err)
-	}
-
-	fmt.Println("----------------Buffer-----------------")
-	// Create a buffer and write to it
 	b := bytes.NewBufferString("Hello ")
 	fmt.Println(b.String())
 	b.WriteString("World")
