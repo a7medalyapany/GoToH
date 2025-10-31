8a66fec51b87e3844586dc4441b5c470068e31c5
a7medalyapany <ahmedalyapany1@gmail.com>
2025-10-31 18:40:01 +0200

    feat: Add factorial calculation using iterative approach

--- 072-recursion/main.go
+++ 072-recursion/main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	x := factorial(5)
 	fmt.Println("Factorial 5 = ", x)
+
+	y := factLoop(4)
+	fmt.Println("Factorial 4 = ", y)
 }
 
 func factorial(n int) int {
@@ -13,4 +16,13 @@ func factorial(n int) int {
 	}
 
 	return n * factorial(n-1)
+}
+
+func factLoop(n int) int {
+	total := 1
+	for n > 0 {
+		total *= n
+		n--
+	}
+	return total
 }
