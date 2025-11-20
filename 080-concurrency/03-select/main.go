44843d1ac5de4871bc31e6e50163a29faf0febb9
a7medalyapany <ahmedalyapany1@gmail.com>
2025-11-20 20:30:59 +0200

    feat: Add main.go with example of goroutine communication using select statement

---
+++ 080-concurrency/03-select/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	eve := make(chan int)
+	odd := make(chan int)
+	quit := make(chan int)
+
+	//send
+	go send(eve, odd, quit)
+	//receive
+	receive(eve, odd, quit)
+
+	fmt.Println("about to exit")
+}
+
+func send(e, o, q chan<- int) {
+	for i := range 100 {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	q <- 0
+}
+
+func receive(e, o, q <-chan int) {
+	for {
+		select {
+		case v := <-e:
+			fmt.Println("from eve channel:", v)
+		case v := <-o:
+			fmt.Println("from odd channel:", v)
+		case v := <-q:
+			fmt.Println("from quit channel:", v)
+			return
+		}
+	}
+}
