7491566e8f7a07d844b6faacc0c89d970e73a1f1
a7medalyapany <ahmedalyapany1@gmail.com>
2025-12-03 03:28:09 +0200

    feat: Implement main function for dog years conversion

---
+++ 086-ninja-level-13/01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"GoToH/084-ninja-level-12/01/dog"
+	"fmt"
+)
+
+
+func main() {
+	fmt.Println("Dog years conversion module")
+	fmt.Println("your dog years:", dog.Years(23))
+}
