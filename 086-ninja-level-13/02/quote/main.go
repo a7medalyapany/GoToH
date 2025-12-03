28182b3d7278a06bb44b7a549d634a3ab3545f07
a7medalyapany <ahmedalyapany1@gmail.com>
2025-12-03 04:02:44 +0200

    feat: Add quote package with English and Arabic quotes

---
+++ 086-ninja-level-13/02/quote/main.go
@@ -0,0 +1,8 @@
+// Package quote provides famous quotes.
+package quote
+
+// Quote is a famous quote in English.
+const Quote = "The only true wisdom is in knowing you know nothing. - Socrates"
+
+// ArQuote is the Arabic translation of the famous quote.
+const ArQuote = "من عرف نفسه فقد عرف ربه. - سقراط"
