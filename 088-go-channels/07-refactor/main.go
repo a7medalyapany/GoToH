0d0f38f07e49065f69feba64fdf179e4514d665b
a7medalyapany <ahmedalyapany1@gmail.com>
2025-12-21 02:56:09 +0200

    feat: Add main.go for incrementor and puller functions with goroutines and channels

---
+++ 088-go-channels/07-refactor/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := incrementor()
+
+	for n := range puller(c) {
+		fmt.Println(n)
+	}
+}
+
+func incrementor() <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for i := 10; i <= 15; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+	return out
+
+}
+
+func puller(c <-chan int) <-chan int {
+	out := make(chan int)
+
+	var sum int
+
+	go func() {
+		for n := range c {
+			sum += n
+		}
+		out <- sum
+		close(out)
+	}()
+
+	return out
+}
